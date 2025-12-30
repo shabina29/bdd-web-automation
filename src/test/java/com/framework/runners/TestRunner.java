2903609f8aaf51e6ec0ace42e07bae8c288ce5fa
Shabina Shahin <sshahinece@gmail.com>
2025-12-30 23:48:32 +0530

    Fix TestRunner to use Cucumber TestNG with Jenkins reports

--- src/test/java/com/framework/runners/TestRunner.java
+++ src/test/java/com/framework/runners/TestRunner.java
@@ -5,15 +5,19 @@ import io.cucumber.testng.CucumberOptions;
 
 @CucumberOptions(
         features = "src/test/resources/features",
-        glue = {
-                "com.framework.steps",
-                "com.framework.base"
+        glue = "com.framework.steps",
+        tags = "not @Ignore",
+        plugin = {
+                "pretty",
+                "html:target/cucumber-reports/index.html",
+                "json:target/cucumber-reports/cucumber.json",
+                "junit:target/cucumber-reports/cucumber.xml"
         },
-        plugin = { "pretty" },
         monochrome = true
 )
 public class TestRunner extends AbstractTestNGCucumberTests {
 }
+
 
 
 
